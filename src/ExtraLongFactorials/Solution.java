21b80aeb0e8d0effd5e6c4123ed7bf94cac5a905
Bùi Đình Hải <hai.bd.hp@gmail.com>
2022-08-12 17:03:37 +0700

    Add some solution #5

---
+++ src/ExtraLongFactorials/Solution.java
@@ -0,0 +1,44 @@
+package ExtraLongFactorials;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'extraLongFactorials' function below.
+     *
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static void extraLongFactorials(int n) {
+        // Write your code here
+        BigInteger result = BigInteger.ONE;
+        for (int i = 1; i <= n ; i++) {
+            result = result.multiply(BigInteger.valueOf(i));
+        }
+        System.out.println(result);
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        Result.extraLongFactorials(n);
+
+        bufferedReader.close();
+    }
+}
---
+++ src/ModifiedKaprekarNumbers/Solution.java
@@ -0,0 +1,72 @@
+package ModifiedKaprekarNumbers;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'kaprekarNumbers' function below.
+     *
+     * The function accepts following parameters:
+     *  1. INTEGER p
+     *  2. INTEGER q
+     */
+
+    public static void kaprekarNumbers(int p, int q) {
+        // Write your code here
+        boolean flag = false;//Denotes if a Kaprekar number has been found
+
+        if(p == 1)//Handle the one case where a 1 digit square is a Kaprekar
+        {
+            System.out.print(p+" ");
+            flag = true;
+        }
+
+
+        for(long i = p; i <= q; i++)
+        {
+            long squareLong = i * i;
+            String squareStr = String.valueOf(squareLong);
+
+            if(squareStr.length() > 1)
+            {
+                long left = Long.parseLong(squareStr.substring(0,(int)Math.ceil(squareStr.length()/2)));
+                long right = Long.parseLong(squareStr.substring((int)Math.ceil(squareStr.length()/2), squareStr.length()));
+                if(left+right == i)
+                {
+                    System.out.print(i+" ");
+                    flag = true;
+                }
+            }
+        }
+        if(!flag)
+        {
+            System.out.println("INVALID RANGE");
+        }
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int p = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        Result.kaprekarNumbers(p, q);
+
+        bufferedReader.close();
+    }
+}
